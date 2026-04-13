0400223c48fda2c6f4f1bac61714127229792927
Christopher Davenport <9170087+42HLFGGK@users.noreply.github.com>
2026-04-13 12:25:01 -0500

    Set up local config and skill matrix UI

---
+++ local-config.example.js
@@ -0,0 +1,7 @@
+window.LOCAL_CONFIG = {
+  token: 'ODRkM2MxYTUtYzdiYi00NjcyLWI4NzEtNTE0MGU0Mjg1MDVjZjQ1Njg3ZGQtZGIx_PF84_183361eb-b803-4a69-8814-ae4432e025f7',
+  orgId: '183361eb-b803-4a69-8814-ae4432e025f7',
+  baseUrl: 'https://api.wxcc-us1.cisco.com',
+  refreshMs: 30000,
+  darkmode: false,
+}
